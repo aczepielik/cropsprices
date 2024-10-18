cfa476bc4419c3830f84fd5a2c9b398a6a36b41a
Adam Czepielik <adam.czepielik@gmail.com>
2024-10-18 20:18:23 +0200

    fixed dataset initialization

--- cloud_init/init_dataset.py
+++ cloud_init/init_dataset.py
@@ -5,10 +5,8 @@ from google.cloud import logging as cloud_logging
 
 # Initialize Google Cloud Logging client
 logging_client = cloud_logging.Client()
-
-# Get the default logger
-logger = logging_client.logger("Cloud Init")
-logger.level = logging.INFO
+logging_client.setup_logging()
+logger = logging.getLogger("Cloud Init")
 
 # Initialize a BigQuery client
 client = bigquery.Client()
@@ -17,4 +15,6 @@ client = bigquery.Client()
 dataset = client.create_dataset("cropsprices_core")
 
 # Log dataset creation
-logger.info(f"Created dataset {dataset.project}.{dataset.dataset_id}")
+logger.info(
+    f"Created dataset {dataset.project}.{dataset.dataset_id}",
+)
