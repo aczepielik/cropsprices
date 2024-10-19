c33f3b2acc7dfb5c0ea32520f81b9d861d5e46dc
Adam Czepielik <adam.czepielik@gmail.com>
2024-10-19 21:05:20 +0200

    added script to download archival data

---
+++ cloud_init/bulk_get_resources.py
@@ -0,0 +1,109 @@
+from typing import Any, Dict, List
+
+import requests
+from google.cloud import bigquery, secretmanager, storage
+from google.cloud import logging as cloud_logging
+from pydantic import ValidationError
+from tqdm import tqdm
+
+from cropsprices.apiquery import query_paged_api
+from cropsprices.models import Resource
+
+
+class ResourceManager:
+    def __init__(self):
+        self.logging_client = cloud_logging.Client()
+        self.logger = self.logging_client.logger("Bulk Data Load")
+        self.bq_client = bigquery.Client()
+        self.storage_client = storage.Client()
+        self.secret_client = secretmanager.SecretManagerServiceClient()
+
+        self.dataset_id = "cropsprices_core"
+        self.table_id = "resources"
+        self.dataset_ref = self.bq_client.dataset(self.dataset_id)
+        self.table_ref = self.dataset_ref.table(self.table_id)
+
+        self.bucket = self._get_gcs_bucket()
+
+    def _get_gcs_bucket(self):
+        secret_name = "projects/cropsprices/secrets/bucket-name/versions/latest"
+        response = self.secret_client.access_secret_version(
+            request={"name": secret_name}
+        )
+        bucket_name = response.payload.data.decode("UTF-8")
+        return self.storage_client.bucket(bucket_name)
+
+    def filter_and_validate_resources(
+        self, resources: List[Dict[str, Any]]
+    ) -> List[Resource]:
+        filtered_resources = []
+        for resource in resources:
+            try:
+                r = Resource(**resource)
+                filtered_resources.append(r)
+            except ValidationError as e:
+                self.logger.error(f"Validation error for resource: {e}")
+        return filtered_resources
+
+    def write_to_bigquery(self, resources: List[Resource]):
+        rows_to_insert = [resource.model_dump() for resource in resources]
+        errors = self.bq_client.insert_rows_json(self.table_ref, rows_to_insert)
+        if errors:
+            self.logger.error(f"Encountered errors while inserting rows: {errors}")
+        else:
+            self.logger.info(
+                f"Successfully inserted {len(rows_to_insert)} rows into BigQuery"
+            )
+
+    def upload_xlsx_to_gcs(self, resources: List[Resource]):
+        xlsx_files = [
+            file
+            for resource in resources
+            for file in resource.attributes.files
+            if file.format.lower() == "xlsx"
+        ]
+
+        with tqdm(total=len(xlsx_files), desc="Uploading XLSX files to GCS") as pbar:
+            for file in xlsx_files:
+                self._process_xlsx_file(file, pbar)
+
+    def _process_xlsx_file(self, file, pbar):
+        try:
+            response = self._download_file(file.download_url)
+            self._upload_to_gcs(file, response.content)
+        except Exception as e:
+            self.logger.error(f"Error processing file {file.download_url}: {str(e)}")
+        finally:
+            pbar.update(1)
+
+    def _download_file(self, url):
+        response = requests.get(url, timeout=30)
+        response.raise_for_status()
+        return response
+
+    def _upload_to_gcs(self, file, content):
+        blob_name = f"wholesale_prices_workbooks/{file.download_url.split('/')[-2]}"
+        blob = self.bucket.blob(blob_name)
+        blob.upload_from_string(content)
+        self.logger.info(f"Uploaded {file.url} to GCS at {blob.public_url}")
+
+    def process_resources(self, url: str, params: Dict[str, str]):
+        all_resources = query_paged_api(url, params)
+        filtered_resources = self.filter_and_validate_resources(all_resources)
+
+        self.write_to_bigquery(filtered_resources)
+        self.upload_xlsx_to_gcs(filtered_resources)
+
+
+def main():
+    url = "https://api.dane.gov.pl/1.4/datasets/912,zintegrowany-system-rolniczej-informacji-rynkowej-biuletyny-informacyjne-rynek-owocow-i-warzyw-swiezych/resources"
+    params = {
+        "sort": "-modified",
+        "title[prefix]": "ceny hurtowe i targowiskowe",
+    }
+    resource_manager = ResourceManager()
+    resource_manager.process_resources(url, params)
+
+
+if __name__ == "__main__":
+    main()
