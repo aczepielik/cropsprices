ce402e9567585b0a83530b481ee8ea0844a77b3b
Adam Czepielik <adam.czepielik@gmail.com>
2024-10-19 21:05:02 +0200

    added initialization of table storing api responses

--- cloud_init/init_resources_table.py
+++ cloud_init/init_resources_table.py
@@ -1,11 +1,13 @@
+import logging
+
 from google.cloud import bigquery
 from google.cloud import logging as cloud_logging
 
 # Initialize Google Cloud Logging client
+# Initialize Google Cloud Logging client
 logging_client = cloud_logging.Client()
-
-# Get the default logger
-logger = logging_client.logger('resources_table_creation')
+logging_client.setup_logging()
+logger = logging.getLogger("Cloud Init")
 
 # Initialize a BigQuery client
 client = bigquery.Client()
@@ -26,39 +28,57 @@ schema = [
     bigquery.SchemaField("media_type", "STRING"),
     bigquery.SchemaField("downloads_count", "INTEGER"),
     bigquery.SchemaField("description", "STRING"),
-    bigquery.SchemaField("regions", "RECORD", mode="REPEATED", fields=[
-        bigquery.SchemaField("region_id", "STRING"),
-        bigquery.SchemaField("hierarchy_label", "STRING"),
-        bigquery.SchemaField("name", "STRING"),
-    ]),
+    bigquery.SchemaField(
+        "regions",
+        "RECORD",
+        mode="REPEATED",
+        fields=[
+            bigquery.SchemaField("region_id", "STRING"),
+            bigquery.SchemaField("hierarchy_label", "STRING"),
+            bigquery.SchemaField("name", "STRING"),
+        ],
+    ),
     bigquery.SchemaField("download_url", "STRING"),
     bigquery.SchemaField("file_size", "INTEGER"),
     bigquery.SchemaField("views_count", "INTEGER"),
-    bigquery.SchemaField("files", "RECORD", mode="REPEATED", fields=[
-        bigquery.SchemaField("file_size", "INTEGER"),
-        bigquery.SchemaField("format", "STRING"),
-        bigquery.SchemaField("openness_score", "INTEGER"),
-        bigquery.SchemaField("download_url", "STRING"),
-    ]),
+    bigquery.SchemaField(
+        "files",
+        "RECORD",
+        mode="REPEATED",
+        fields=[
+            bigquery.SchemaField("file_size", "INTEGER"),
+            bigquery.SchemaField("format", "STRING"),
+            bigquery.SchemaField("openness_score", "INTEGER"),
+            bigquery.SchemaField("download_url", "STRING"),
+        ],
+    ),
     bigquery.SchemaField("language", "STRING"),
     bigquery.SchemaField("created", "TIMESTAMP"),
     bigquery.SchemaField("file_url", "STRING"),
-    bigquery.SchemaField("institution", "RECORD", fields=[
-        bigquery.SchemaField("id", "STRING"),
-        bigquery.SchemaField("type", "STRING"),
-    ]),
-    bigquery.SchemaField("dataset", "RECORD", fields=[
-        bigquery.SchemaField("id", "STRING"),
-        bigquery.SchemaField("type", "STRING"),
-    ]),
+    bigquery.SchemaField(
+        "institution",
+        "RECORD",
+        fields=[
+            bigquery.SchemaField("id", "STRING"),
+            bigquery.SchemaField("type", "STRING"),
+        ],
+    ),
+    bigquery.SchemaField(
+        "dataset",
+        "RECORD",
+        fields=[
+            bigquery.SchemaField("id", "STRING"),
+            bigquery.SchemaField("type", "STRING"),
+        ],
+    ),
 ]
 
 # Set up the table reference
-table_id = "cropsprices.your_dataset.resources"
+table_id = "cropsprices.cropsprices_core.resources"
 table = bigquery.Table(table_id, schema=schema)
 
 # Create the table
 table = client.create_table(table)
 
 # Log the table creation
-logger.log_text(f"Created table {table.project}.{table.dataset_id}.{table.table_id}")
+logger.info(f"Created table {table.project}.{table.dataset_id}.{table.table_id}")
