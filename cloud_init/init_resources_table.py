1409393466fec48f229ede6941cceeb0fdce46a3
Adam Czepielik <adam.czepielik@gmail.com>
2024-10-19 23:11:07 +0200

    fixed schema of resources table

--- cloud_init/init_resources_table.py
+++ cloud_init/init_resources_table.py
@@ -4,7 +4,6 @@ from google.cloud import bigquery
 from google.cloud import logging as cloud_logging
 
 # Initialize Google Cloud Logging client
-# Initialize Google Cloud Logging client
 logging_client = cloud_logging.Client()
 logging_client.setup_logging()
 logger = logging.getLogger("Cloud Init")
@@ -16,59 +15,116 @@ client = bigquery.Client()
 schema = [
     bigquery.SchemaField("id", "STRING", mode="REQUIRED"),
     bigquery.SchemaField("type", "STRING"),
-    bigquery.SchemaField("format", "STRING"),
-    bigquery.SchemaField("title", "STRING"),
-    bigquery.SchemaField("is_chart_creation_blocked", "BOOLEAN"),
-    bigquery.SchemaField("openness_score", "INTEGER"),
-    bigquery.SchemaField("contains_protected_data", "BOOLEAN"),
-    bigquery.SchemaField("data_date", "DATE"),
-    bigquery.SchemaField("link", "STRING"),
-    bigquery.SchemaField("modified", "TIMESTAMP"),
-    bigquery.SchemaField("verified", "TIMESTAMP"),
-    bigquery.SchemaField("media_type", "STRING"),
-    bigquery.SchemaField("downloads_count", "INTEGER"),
-    bigquery.SchemaField("description", "STRING"),
     bigquery.SchemaField(
-        "regions",
+        "attributes",
         "RECORD",
-        mode="REPEATED",
         fields=[
-            bigquery.SchemaField("region_id", "STRING"),
-            bigquery.SchemaField("hierarchy_label", "STRING"),
-            bigquery.SchemaField("name", "STRING"),
-        ],
-    ),
-    bigquery.SchemaField("download_url", "STRING"),
-    bigquery.SchemaField("file_size", "INTEGER"),
-    bigquery.SchemaField("views_count", "INTEGER"),
-    bigquery.SchemaField(
-        "files",
-        "RECORD",
-        mode="REPEATED",
-        fields=[
-            bigquery.SchemaField("file_size", "INTEGER"),
             bigquery.SchemaField("format", "STRING"),
+            bigquery.SchemaField("title", "STRING"),
+            bigquery.SchemaField("is_chart_creation_blocked", "BOOLEAN"),
+            bigquery.SchemaField("has_research_data", "BOOLEAN"),
             bigquery.SchemaField("openness_score", "INTEGER"),
+            bigquery.SchemaField("contains_protected_data", "BOOLEAN"),
+            bigquery.SchemaField("supplements", "STRING", mode="REPEATED"),
+            bigquery.SchemaField("data_date", "DATE"),
+            bigquery.SchemaField("has_high_value_data", "BOOLEAN"),
+            bigquery.SchemaField("has_dynamic_data", "BOOLEAN"),
+            bigquery.SchemaField("link", "STRING"),
+            bigquery.SchemaField("csv_download_url", "STRING"),
+            bigquery.SchemaField("csv_file_url", "STRING"),
+            bigquery.SchemaField("modified", "TIMESTAMP"),
+            bigquery.SchemaField("visualization_types", "STRING", mode="REPEATED"),
+            bigquery.SchemaField("verified", "TIMESTAMP"),
+            bigquery.SchemaField("media_type", "STRING"),
+            bigquery.SchemaField("special_signs", "STRING", mode="REPEATED"),
+            bigquery.SchemaField("downloads_count", "INTEGER"),
+            bigquery.SchemaField("description", "STRING"),
+            bigquery.SchemaField(
+                "regions",
+                "RECORD",
+                mode="REPEATED",
+                fields=[
+                    bigquery.SchemaField("region_id", "STRING"),
+                    bigquery.SchemaField("hierarchy_label", "STRING"),
+                    bigquery.SchemaField("name", "STRING"),
+                ],
+            ),
             bigquery.SchemaField("download_url", "STRING"),
+            bigquery.SchemaField("jsonld_download_url", "STRING"),
+            bigquery.SchemaField("file_size", "INTEGER"),
+            bigquery.SchemaField("jsonld_file_size", "INTEGER"),
+            bigquery.SchemaField("views_count", "INTEGER"),
+            bigquery.SchemaField("jsonld_file_url", "STRING"),
+            bigquery.SchemaField("csv_file_size", "INTEGER"),
+            bigquery.SchemaField(
+                "files",
+                "RECORD",
+                mode="REPEATED",
+                fields=[
+                    bigquery.SchemaField("file_size", "INTEGER"),
+                    bigquery.SchemaField("format", "STRING"),
+                    bigquery.SchemaField("openness_score", "INTEGER"),
+                    bigquery.SchemaField("download_url", "STRING"),
+                ],
+            ),
+            bigquery.SchemaField("language", "STRING"),
+            bigquery.SchemaField("created", "TIMESTAMP"),
+            bigquery.SchemaField("file_url", "STRING"),
         ],
     ),
-    bigquery.SchemaField("language", "STRING"),
-    bigquery.SchemaField("created", "TIMESTAMP"),
-    bigquery.SchemaField("file_url", "STRING"),
     bigquery.SchemaField(
-        "institution",
+        "relationships",
         "RECORD",
         fields=[
-            bigquery.SchemaField("id", "STRING"),
-            bigquery.SchemaField("type", "STRING"),
+            bigquery.SchemaField(
+                "institution",
+                "RECORD",
+                fields=[
+                    bigquery.SchemaField(
+                        "data",
+                        "RECORD",
+                        fields=[
+                            bigquery.SchemaField("type", "STRING"),
+                            bigquery.SchemaField("id", "STRING"),
+                        ],
+                    ),
+                    bigquery.SchemaField(
+                        "links",
+                        "RECORD",
+                        fields=[
+                            bigquery.SchemaField("related", "STRING"),
+                        ],
+                    ),
+                ],
+            ),
+            bigquery.SchemaField(
+                "dataset",
+                "RECORD",
+                fields=[
+                    bigquery.SchemaField(
+                        "data",
+                        "RECORD",
+                        fields=[
+                            bigquery.SchemaField("type", "STRING"),
+                            bigquery.SchemaField("id", "STRING"),
+                        ],
+                    ),
+                    bigquery.SchemaField(
+                        "links",
+                        "RECORD",
+                        fields=[
+                            bigquery.SchemaField("related", "STRING"),
+                        ],
+                    ),
+                ],
+            ),
         ],
     ),
     bigquery.SchemaField(
-        "dataset",
+        "links",
         "RECORD",
         fields=[
-            bigquery.SchemaField("id", "STRING"),
-            bigquery.SchemaField("type", "STRING"),
+            bigquery.SchemaField("self", "STRING"),
         ],
     ),
 ]
